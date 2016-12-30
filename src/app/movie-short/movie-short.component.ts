5a5c64483212a064e1479c97a2ee3d434a354596
Dimitar Belchugov <dimitar.belchugov@madbid.com>
2016-12-30 14:35:53 +0200

    Add movie short component

--- src/app/movie-short/movie-short.component.ts
+++ src/app/movie-short/movie-short.component.ts
@@ -1,12 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+
+import { Movie } from '../models/movie.interface';
 
 @Component({
-  selector: 'app-movie-short',
+  selector: '[app-movie-short]',
   templateUrl: './movie-short.component.html',
   styleUrls: ['./movie-short.component.css']
 })
 export class MovieShortComponent implements OnInit {
+  @Input() movie: Movie;
 
+  get title(): string {
+    return this.movie.Title;
+  }
   constructor() { }
 
   ngOnInit() {
